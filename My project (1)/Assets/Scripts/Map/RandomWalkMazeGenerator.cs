70292559e50c33bd8f42dd0e8733b37aa706ea5e
Dimitrios <dimitriosbroumas@gmai.com>
2025-01-25 22:34:11 -0500

    BPS 75%

--- My project (1)/Assets/Scripts/Map/RandomWalkMazeGenerator.cs
+++ My project (1)/Assets/Scripts/Map/RandomWalkMazeGenerator.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering.Universal;
+using Random = UnityEngine.Random;
 
 public static class RandomWalkMazeGenerator 
 {
@@ -33,7 +35,69 @@ public static class RandomWalkMazeGenerator
         }
         return corridorPath;
     }
-    
+
+    public static List<BoundsInt> BinarySpacePartitioning(BoundsInt spaceToSplit, int minwidth, int minheight)
+    {
+        Queue<BoundsInt> roomsQueue = new Queue<BoundsInt>();
+        List<BoundsInt> roomsList = new List<BoundsInt>();
+        roomsQueue.Enqueue(spaceToSplit);
+
+        while(roomsQueue.Count > 0)
+        {
+            var room = roomsQueue.Dequeue();
+            if(room.size.y >= minheight && room.size.x >= minwidth)
+            {
+                if(Random.value < 0.5f)
+                {
+                    if(room.size.y >= minheight * 2)
+                    {
+                        SplitHorizontally(minwidth, minheight, roomsQueue, room);
+                    }
+                    else if(room.size.x >= minwidth * 2)
+                    {
+                        SplitVertically(minwidth, minheight, roomsQueue, room);
+                    }
+                    else if(room.size.y >= minheight && room.size.x >= minwidth)
+                    {
+                        roomsList.Add(room);
+                    }
+                }
+                else{
+                    if(room.size.x >= minwidth * 2)
+                    {
+                        SplitVertically(minwidth, minheight, roomsQueue, room);
+                    }
+                    else if(room.size.y >= minheight * 2)
+                    {
+                        SplitHorizontally(minwidth, minheight, roomsQueue, room);
+                    }
+                    else if(room.size.y >= minheight && room.size.x >= minwidth)
+                    {
+                        roomsList.Add(room);
+                    }
+                }
+            }
+        }
+        return roomsList;
+    }
+
+    private static void SplitVertically(int minwidth, int minheight, Queue<BoundsInt> roomsQueue, BoundsInt room)
+    {
+        var xSplit = Random.Range(1, room.size.x);
+        BoundsInt room1 = new BoundsInt(room.min, new Vector3Int(xSplit, room.size.y, room.size.z));
+        BoundsInt room2 = new BoundsInt(new Vector3Int(room.min.x + xSplit, room.min.y, room.min.z), new Vector3Int(room.size.x - xSplit, room.size.y, room.size.z));
+        roomsQueue.Enqueue(room1);
+        roomsQueue.Enqueue(room2);
+    }
+
+    private static void SplitHorizontally(int minwidth, int minheight, Queue<BoundsInt> roomsQueue, BoundsInt room)
+    {
+        var ySplit = Random.Range(1, room.size.y);
+        BoundsInt room1 = new BoundsInt(room.min, new Vector3Int(room.size.x, ySplit, room.size.z));
+        BoundsInt room2 = new BoundsInt(new Vector3Int(room.min.x, room.min.y+ySplit, room.min.z), new Vector3Int(room.size.x, room.size.y-ySplit, room.size.z));
+        roomsQueue.Enqueue(room1);
+        roomsQueue.Enqueue(room2);
+    }
 }
 
 
